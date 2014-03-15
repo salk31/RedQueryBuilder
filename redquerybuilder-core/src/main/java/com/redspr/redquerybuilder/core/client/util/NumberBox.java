aacb8480ecb539fab2dc76a32ce21567173181f2
Sam Hough <sam@redspr.com>
2014-03-15 10:19:22 +0000

    HTML5 friendly number box

--- redquerybuilder-core/src/main/java/com/redspr/redquerybuilder/core/client/util/NumberBox.java
+++ redquerybuilder-core/src/main/java/com/redspr/redquerybuilder/core/client/util/NumberBox.java
@@ -1,25 +1,34 @@
 package com.redspr.redquerybuilder.core.client.util;
 
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.DoubleBox;
-import com.google.gwt.user.client.ui.HasValue;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.text.client.DoubleParser;
+import com.google.gwt.text.shared.AbstractRenderer;
+import com.google.gwt.user.client.ui.ValueBox;
 import com.redspr.redquerybuilder.core.client.engine.Session;
 import com.redspr.redquerybuilder.core.shared.meta.Column;
 import com.redspr.redquerybuilder.core.shared.meta.Editor;
 
-public class NumberBox extends Composite implements HasValue<Double> {
+public class NumberBox extends ValueBox<Double> {
 
     private static final Double ONE = new Double(1.0);
 
-    private final DoubleBox box = new DoubleBox();
+    // HTML5 input=number fields don't like commas etc
+    private static final AbstractRenderer<Double> RENDERER = new AbstractRenderer<Double>() {
+        @Override
+        public String render(Double object) {
+            if (object == null) {
+                return "";
+            }
+            return "" + object;
+        }
+    };
 
     public NumberBox(Session session, Column column) {
-        initWidget(box);
+        super(Document.get().createTextInputElement(), RENDERER,
+                DoubleParser.instance());
 
         // HTML5 browser might pick up on this
-        this.getElement().setAttribute("type", "number");
+        getElement().setAttribute("type", "number");
 
         Editor editor = column.getEditor();
         Double step = (Double) editor.getAttribute("step");
@@ -29,27 +38,8 @@ public class NumberBox extends Composite implements HasValue<Double> {
         getElement().setAttribute("step", step.toString());
         // TODO 05 min, max
         // TODO 05 validators that can contribute html5 values?
-        // TODO 05 step
         // TODO __ just onkeyup validator?
     }
 
-    @Override
-    public HandlerRegistration addValueChangeHandler(ValueChangeHandler<Double> handler) {
-        return box.addValueChangeHandler(handler);
-    }
 
-    @Override
-    public Double getValue() {
-        return box.getValue();
-    }
-
-    @Override
-    public void setValue(Double value) {
-        setValue(value, false);
-    }
-
-    @Override
-    public void setValue(Double value, boolean fireEvents) {
-        box.setValue(value, fireEvents);
-    }
 }
